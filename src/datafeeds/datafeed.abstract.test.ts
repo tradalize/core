07da7d933c8ad51fee4f1c5136ba61cf14f0059c
Vladyslav Bezsmertnyi <besmercj123@gmail.com>
2023-12-23 16:34:57 +0200

    Abstract datafeed

---
+++ src/datafeeds/datafeed.abstract.test.ts
@@ -0,0 +1,30 @@
+import { describe, expect, test } from "vitest";
+import { Datafeed } from ".";
+
+const testArray = [4, 5];
+
+class TestDatafeed extends Datafeed<number> {
+  constructor() {
+    super([1, 2, 3]);
+  }
+
+  public async loadNextChunk() {
+    return testArray.splice(0);
+  }
+}
+
+describe("Datafeed abstract", () => {
+  test("should iterate over given list item by item", async () => {
+    const testDatafeed = new TestDatafeed();
+
+    const resultArray = [];
+
+    while (!testDatafeed.isLast) {
+      const next = await testDatafeed.next();
+
+      resultArray.push(next);
+    }
+
+    expect(resultArray).toStrictEqual([1, 2, 3, 4, 5]);
+  });
+});
---
+++ src/datafeeds/datafeed.abstract.ts
@@ -0,0 +1,47 @@
+import { LinkedList } from "../utils/linkedList";
+
+export type Candle = {
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  openTime: number;
+  closeTime: number;
+  volume: number;
+};
+
+export const enum Timeframe {
+  OneMinute = "1m",
+  FiveMinutes = "5m",
+  FifteenMinutes = "15m",
+  OneHour = "1h",
+  FourHours = "4h",
+  OneDay = "1d",
+  OneWeek = "1w",
+}
+
+export abstract class Datafeed<T = Candle> {
+  list = new LinkedList<T>();
+
+  constructor(data: T[] = []) {
+    this.list.pushBulk(data);
+  }
+
+  public async next(): Promise<T> {
+    const nextItem = this.list.shift();
+
+    if (this.isLast) {
+      this.list.pushBulk(await this.loadNextChunk());
+    }
+
+    return nextItem;
+  }
+
+  public async loadNextChunk(): Promise<T[]> {
+    return [];
+  }
+
+  get isLast(): boolean {
+    return this.list.head === null;
+  }
+}
